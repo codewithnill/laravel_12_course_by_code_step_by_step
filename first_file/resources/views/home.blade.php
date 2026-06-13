4be539323e4449650914df836fc7008869b24fd0
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-13 15:52:01 +0600

    routing using anchor tags.

---
+++ first_file/resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1>Home page no blade</h1>
+<a href="/">Welcome page</a>
+<a href="/about/nbnill">About page</a>
--- first_file/routes/web.php
+++ first_file/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    return view('home');
+    return view('welcome');
+    // return view('home');
 });
 
 //Route::get('/home', function () { if written home after the /, the home page can be accessed by writing localhost:8000/home in the url. 
