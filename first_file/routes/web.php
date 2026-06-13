8451bea87a9374635427a6587d31e28a143be752
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-13 12:41:51 +0600

    Update web.php to set home as the landing page and add comment for accessing via URL.

--- first_file/routes/web.php
+++ first_file/routes/web.php
@@ -6,3 +6,5 @@ Route::get('/', function () {
     // return view('welcome');
     return view('home');
 });
+
+//Route::get('/home', function () { if written home after the /, the home page can be accessed by writing localhost:8000/home in the url. 
