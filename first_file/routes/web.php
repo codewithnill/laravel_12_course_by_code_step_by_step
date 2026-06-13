0bf4e3b17e9c8f2884e8d3296c2eb29cb8d9f350
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-13 15:37:02 +0600

    passing data with routing.

--- first_file/routes/web.php
+++ first_file/routes/web.php
@@ -8,3 +8,15 @@ Route::get('/', function () {
 });
 
 //Route::get('/home', function () { if written home after the /, the home page can be accessed by writing localhost:8000/home in the url. 
+
+// shortcut
+Route::view('/home', 'home'); // this is the shortcut for the above code. It will return the home view when the user visits the /home url.
+
+Route::get('/about',function () {
+    return view('about');
+});
+
+Route::get('/about/{name}',function ($name) {
+    echo $name;
+    return view('about');
+});
