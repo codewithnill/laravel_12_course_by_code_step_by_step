606437a7186b3c4225ef6f5558648c08e7106473
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-18 03:09:45 +0600

    checking if sub view exists.

--- first_file/resources/views/about.blade.php
+++ first_file/resources/views/about.blade.php
@@ -1,6 +1,10 @@
 @include('common.header') 
 @include('common.inner',['page'=>"this is about page"])
 
+<!-- checking if sub view exists -->
+{{-- @include('common.common',['page'=>"this is about page"])
+error message : View [common.common] not found. --}}
+
 <h1>About</h1>
 
 <!-- double braces cause it is a blade.php -->
