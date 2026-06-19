086537a91297b58bed4d049f7ec104a2ca927aa4
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-19 14:21:13 +0600

    making link url with anchor tags.

--- first_file/resources/views/welcomeforurl.blade.php
+++ first_file/resources/views/welcomeforurl.blade.php
@@ -1,5 +1,8 @@
 <div>
     <h1>Welcome page</h1>
-    <a href="/aforurl">About page</a>
+    <!-- <a href="/aforurl">About page</a> -->
+    <!-- or -->
+    <a href="{{URL::to('/aforurl')}}">About page</a>
+    <a href="{{URL::to('/hforurl')}}">Home page</a>
     <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
 </div>
