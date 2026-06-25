2d52686ee3356c9a69a9588e18e7299aefa67ba3
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-26 01:48:49 +0600

    ran migration and get data from db.

--- first_file/app/Http/Controllers/user_controller_for_db.php
+++ first_file/app/Http/Controllers/user_controller_for_db.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class user_controller_for_db extends Controller
 {
     //
     function users() {
-        return "user function";
+        // return "user function";
+        return DB::select('select * from users');
     }
 }
