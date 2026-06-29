d9a92c67eb7fec8a4433a3575629f9df0cecbc63
codewithnill <nbnill.hmlnmh@gmail.com>
2026-06-30 03:22:24 +0600

    making a function in model and use it.

--- first_file/app/Http/Controllers/studentControllerDb.php
+++ first_file/app/Http/Controllers/studentControllerDb.php
@@ -8,6 +8,8 @@ class studentControllerDb extends Controller
 {
     //
     function getStudents() {
+        $data=new \App\Models\student;
+        echo $data->testFun();
         $students=\App\Models\student::all();
         // return $students;
         return view('students',['data'=>$students]);
--- first_file/app/Models/student.php
+++ first_file/app/Models/student.php
@@ -8,4 +8,8 @@ class student extends Model
 {
     //
     // protected $table="college_students"; // prevent error for table names
+
+    function testFun() {
+        return "dummy function called from model.";
+    }
 }
