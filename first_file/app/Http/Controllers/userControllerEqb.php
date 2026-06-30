42eb3741f329917a2afcd5b0fc9fd87c59ea50b6
codewithnill <nbnill.hmlnmh@gmail.com>
2026-07-01 02:30:38 +0600

    using find() to get user with id and inserting in table.

--- first_file/app/Http/Controllers/userControllerEqb.php
+++ first_file/app/Http/Controllers/userControllerEqb.php
@@ -16,10 +16,24 @@ class userControllerEqb extends Controller
         
         // $response=userModelEqb::where('phone','01711111111')->get();
         
-        $response=userModelEqb::where('phone','01711111111')->first();
+        // $response=userModelEqb::where('phone','01711111111')->first();
+
+        // $response=userModelEqb::find(2);
+
         
 
-        $response=[$response];
-        return view('userViewEqb',['users'=>$response]);    
+        // $response=[$response];
+        // return view('userViewEqb',['users'=>$response]);    
+
+
+        $response=userModelEqb::insert(['name'=>'Gay','email'=>'gay@test.com','phone'=>'0101']);
+
+        if($response) {
+            return "Data inserted..";
+        } else {
+            return "Data not inserted..";
+        }
     }
+
+
 }
