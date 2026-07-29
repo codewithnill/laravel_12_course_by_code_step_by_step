ab03657eeeecdc08e571d25e72c7e591468d849e
codewithnill <nbnill.hmlnmh@gmail.com>
2026-07-30 01:29:40 +0600

    checking if direct access to profile has data stored in session or not by using incognito window in chrome

--- first_file/resources/views/profileForSession.blade.php
+++ first_file/resources/views/profileForSession.blade.php
@@ -1,5 +1,11 @@
 <div>
     <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
     <h1>Profile</h1>
-    <h3>{{ session('user') }}</h3>
+    <!-- <h3>{{ session('user') }}</h3> -->
+
+    @if(session('user'))
+        <h3>Welcome, {{ session('user') }}!</h3>
+    @else
+        <h3>No user logged in.</h3>
+    @endif
 </div>
