dc2cd830c82b6943c069f8f430040a46da35341d
codewithnill <nbnill.hmlnmh@gmail.com>
2026-07-30 03:34:06 +0600

    naming uploaded file and storing.

--- first_file/app/Http/Controllers/uploadController.php
+++ first_file/app/Http/Controllers/uploadController.php
@@ -10,7 +10,9 @@ class uploadController extends Controller
     function upload(Request $req) {
         // return view('upload');
         // echo "File uploaded successfully";
-        $path=$req->file('file')->store('public');
+        // $path=$req->file('file')->store('public');
+
+        $path=$req->file('file')->storeAs('public','dummy1.png');
         $filenamearray=explode('/',$path);
         $filename=$filenamearray[1];
         return view('display', ['path'=>$filename]);
