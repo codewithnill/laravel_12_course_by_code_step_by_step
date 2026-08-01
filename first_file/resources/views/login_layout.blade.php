7c183145f8a8f4eb73b846807eea42555e6da583
codewithnill <nbnill.hmlnmh@gmail.com>
2026-08-01 13:00:00 +0600

    login page using the previous same component.

---
+++ first_file/resources/views/login_layout.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-slot name="title">Login</x-slot>
+    <x-slot name="main">
+        <div>
+            <h1>User login</h1>
+            <h3>Sub heading for login page</h3>
+            <input type="text" name="username" placeholder="Enter username">
+            <br><br>
+            <input type="text" name="password" placeholder="Enter password">
+            <br><br>
+            <button type="submit">Login</button>
+        </div>
+    </x-slot>
+</x-layout>
