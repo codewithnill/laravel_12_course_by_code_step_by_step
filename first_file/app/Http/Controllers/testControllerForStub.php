42ca7775445d2b7d06de40f3d349784bcf85a8df
codewithnill <nbnill.hmlnmh@gmail.com>
2026-08-02 01:40:03 +0600

    stub customization.

---
+++ first_file/app/Http/Controllers/testControllerForStub.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testControllerForStub extends Controller
+{
+    //
+    function index(Request $req) {
+        return "some code";
+    }
+}
