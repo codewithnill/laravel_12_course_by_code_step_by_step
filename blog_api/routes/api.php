0dc3b367fefd313632baa0e22373067ed3f52cda
codewithnill <nbnill.hmlnmh@gmail.com>
2026-08-08 21:56:10 +0600

    api authentication with laravel sanctum.

--- blog_api/routes/api.php
+++ blog_api/routes/api.php
@@ -15,20 +15,36 @@ Route::get("/test",function(){
     return ["name"=>"NB Nill","dept"=>"CSE"];
 });
 
-Route::get('students',[studentController::class,'list']);
-Route::post('add_students',[studentController::class,'add']);
 
-
-Route::put('update_students',[studentController::class,'update']);
-
-Route::delete('delete_students/{id}',[studentController::class,'deleteStudent']);
 
 
 Route::resource('members', memberController::class);
 
 
 
-// all above routes were without token authentication. Now we will add token authentication for the following routes
 
 Route::post('login',[userAuthController::class,'login']);
 Route::post('signup',[userAuthController::class,'signup']);
+
+
+
+// Route::get('students',[studentController::class,'list']);
+// Route::post('add_students',[studentController::class,'add']);
+// Route::put('update_students',[studentController::class,'update']);
+// Route::delete('delete_students/{id}',[studentController::class,'deleteStudent']);
+
+// all above routes were without token authentication. Now we will add token authentication for the following routes
+
+
+
+// creating a middleware group for the routes that require token authentication
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('students',[studentController::class,'list']);
+    Route::post('add_students',[studentController::class,'add']);
+    Route::put('update_students',[studentController::class,'update']);
+    Route::delete('delete_students/{id}',[studentController::class,'deleteStudent']);
+    Route::get('search-student/{name}',[studentController::class,'searchStudent']);
+});
+
+Route::get('login',[userAuthController::class,'login'])->name('login');
+
