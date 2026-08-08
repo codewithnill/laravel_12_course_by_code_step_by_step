39d1e3e4c5ce0933582b590b07a47c22b57d700e
codewithnill <nbnill.hmlnmh@gmail.com>
2026-08-08 21:00:26 +0600

    login api with laravel sanctum.

--- blog_api/app/Http/Controllers/userAuthController.php
+++ blog_api/app/Http/Controllers/userAuthController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class userAuthController extends Controller
 {
     //
     function login(Request $request){
-       return "login function called";
+    //    return $request->all();
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return ["result" => "user not found", "success" => false, "message" => "Invalid email or password"];
+        }
+
+        $success["token"] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+        return ['success' => true, "result" => $success, "message" => "User logged in successfully"];
     }
 
     function signup(Request $request){
